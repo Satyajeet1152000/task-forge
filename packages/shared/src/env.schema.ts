1c7689f0530aeab7b46899fe4ed06574e82b0406
satyajeet1152000 <satyajeet11520000@gmail.com>
2026-05-21 15:18:48 +0530

    fixed backend

--- packages/shared/src/env.schema.ts
+++ packages/shared/src/env.schema.ts
@@ -4,7 +4,6 @@ const envSchema = z.object({
   APP_NAME: z.string(),
   NODE_ENV: z.enum(["development", "production"]),
   PORT: z.coerce.number(),
-  FRONTEND_URL: z.string().url(),
 
   DATABASE_HOST: z.string(),
   DATABASE_PORT: z.coerce.number(),
